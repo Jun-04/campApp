b54fd77c0b9fcd5af718240867be9df319ae6f4d
jun <pinetreee.japan@gmail.com>
2025-11-19 11:43:34 +1100

    feat: Implement data validation for show route and update layout and installing Boot Strap

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: '../.env'});
+require('dotenv').config({ path: __dirname + '/../.env' });
 const cities = require('./cities');
 const mongoose = require('mongoose');
 const { places, descriptors } = require('./seedHelper'); 
@@ -19,10 +19,14 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
-        const  random1000 = Math.floor(Math.random() * 1000);
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title:` ${sample(descriptors)} ${sample(places)}`
+            title:` ${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur',
+            price
         })
         await camp.save();
     }
