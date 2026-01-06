7a2eaf4014dc33cedebae975b26194f9fd2e2677
jun <pinetreee.japan@gmail.com>
2026-01-07 08:36:51 +1100

    Refactor: Add auth middlewares and authorization for campgrounds

--- middleware.js
+++ middleware.js
@@ -1,3 +1,7 @@
+const { campgroundSchema, reviewSchema } = require("./schemas.js");
+const Campground = require('./models/campground.js')
+const ExpressError = require('./utils/ExpressError.js');
+
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.returnTo = req.originalUrl;
@@ -13,3 +17,36 @@ module.exports.storeReturnTo = (req, res, next) => {
     }
     next();
 }
+
+
+//validation middlewear
+module.exports.validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(", ");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+//Router protect middlewear
+module.exports.isAuthor = async ( req, res, next ) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+    if(!campground.author.equals(req.user._id)) {
+      req.flash('error', 'You do not Have permission to do that');
+      return res.redirect(`/campgrounds/${id}`)
+    }
+    next();
+}
+
+module.exports.validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(", ");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,23 +1,13 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-
 const Campground = require("../models/campground");
 const Review = require("../models/review.js");
+const { validateReview } = require('../middleware.js');
 
-const { reviewSchema } = require("../schemas.js");
 
 const ExpressError = require("../utils/ExpressError.js");
 const catchAsync = require("../utils/catchAsync");
 
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(", ");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
 
 router.post(
   "/",
